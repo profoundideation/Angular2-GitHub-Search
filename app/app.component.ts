d058415e7f76a57c5c0d710febf20cdcbcab6498
Daniel Lim <hbt@markhamboards.com>
2016-08-06 15:27:37 -0400

    rapid fire
    rapid fire

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html',
+
 })
 export class AppComponent { }
