60a419ab5cbb4c3fbe87b659f507537d8df494da
Daniel Lim <hbt@markhamboards.com>
2016-08-06 15:32:58 -0400

    add App Components
    Adding App Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,9 @@
 import {Component} from '@angular/core';
-
+import {ProfileComponent} from './components/profile.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
-
+    directives:[ProfileComponent]
 })
 export class AppComponent { }
