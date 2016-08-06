c0fd140b5c7d602fec731f9dddb2b75f50a64ea9
Daniel Lim <hbt@markhamboards.com>
2016-08-06 19:29:25 -0400

    End of Part 2
    End of Part 2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # [Angular 2] GitHub Profile Search Web App.
 
-## Check out my <a href="profoundideation.com/GitHubSearchR/">[ReactJS] GitHub Profile Search Web App</a> too.
+## Check out my [[ReactJS] GitHub Profile Search Web App](http://profoundideation.com/GitHubSearchR/) too.
--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -1,16 +1,16 @@
-import {Injectable} from '@angular/core';
-import {Http, Headers} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService {
   private username; // Github Username
 
-  constructor(private _http:Http){
+  constructor(private _http:Http) {
     this.username = 'profoundhub';
   }
 
-  getUser(){
+  getUser() {
     return this._http.get('https://api.github.com/users/'+this.username)
       .map(res => res.json());
   }
